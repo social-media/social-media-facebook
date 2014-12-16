168c5fc423875b20efcae494a2cdb44cf6b73c82
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-16 14:56:10 +0100

    Example updated

--- examples/example.php
+++ examples/example.php
@@ -13,10 +13,19 @@ use JeroenDesloovere\SocialMedia\Facebook\Objects\Service as FacebookService;
 // define api
 $api = new SocialMedia();
 
+use SocialMedia\SocialMedia as SocialMedia;
+use SocialMedia\Facebook\Objects\Post as FacebookPost;
+use SocialMedia\Facebook\Objects\Service as FacebookService;
+
+// define api
+$api = new SocialMedia();
+
 // define external post objects
-$service = new FacebookService();
 $post = new FacebookPost();
+$credentials = new FacebookCredentials();
+$api = array(); // todo
+$service = new FacebookService($api, $credentials);
 
 // (un)publish a message
-$api->timeline->publish($service, $post);
-$api->timeline->unpublish($service, $post);
+$api->getTimeline()->publish($service, $post);
+$api->getTimeline()->unpublish($service, $post);
