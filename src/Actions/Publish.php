15d4200f1ae9c8e90a014d4363d617c566d56fc3
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-16 14:55:35 +0100

    Two actions added

---
+++ src/Actions/Publish.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SocialMedia\Facebook\Actions;
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use SocialMedia\Facebook\Objects\Service as FacebookService;
+use SocialMedia\Facebook\Objects\Post as FacebookPost;
+
+/**
+ * Publish Post to Facebook.
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class Publish
+{
+    
+}
