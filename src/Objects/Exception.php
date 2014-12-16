d816e33596c964bd5cbf380cfa634728a7d4a61f
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-16 15:58:24 +0100

    Exception moved

---
+++ src/Objects/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace SocialMedia\Facebook\Objects;
+
+use SocialMedia\Core\Objects\Exception as CoreException;
+
+/**
+ * Social Media Facebook Exception class
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class Exception extends CoreException
+{
+}
