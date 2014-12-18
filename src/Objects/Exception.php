aa44e3f1d9b7cad8e94a2b9f49d979b3a050e95e
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-18 10:37:15 +0100

    Copyright added to Exception

--- src/Objects/Exception.php
+++ src/Objects/Exception.php
@@ -1,5 +1,13 @@
 <?php
+
 namespace SocialMedia\Facebook\Objects;
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
 
 use SocialMedia\Core\Objects\Exception as CoreException;
 
