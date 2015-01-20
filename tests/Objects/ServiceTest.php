018eac1418564bcb5e0ba81107b84012c072a7a8
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-01-20 15:39:11 +0100

    ServiceTest added

---
+++ tests/Objects/ServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SocialMedia\Facebook\tests\Objects;
+
+// required to load
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use SocialMedia\Facebook\Objects\Service as FacebookService;
+use SocialMedia\Facebook\Objects\Credentials as FacebookCredentials;
+
+/**
+ * Credentials Class which contains all credentials
+ * to post to the Facebook service.
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class ServiceTest extends \PHPUnit_Framework_TestCase
+{
+    protected static $service;
+
+    /**
+     * Set up extra variables
+     */
+    protected function setUp()
+    {
+        $this->service = new FacebookService(
+            array(),
+            new FacebookCredentials()
+        );
+    }
+
+    /**
+     * Tear down after class
+     */
+    public function tearDown()
+    {
+        $this->service = null;
+    }
+
+    /**
+     * Test service
+     */
+    public function testService()
+    {
+        /*
+        $accessToken = 'testAccessToken';
+        $this->credentials->setAccessToken($accessToken);
+        $this->assertEquals($accessToken, $this->credentials->getAccessToken());
+        */
+    }
+}
