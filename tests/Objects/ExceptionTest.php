9e1d57833f4390f2ae5a299e6873b0a1c54570e7
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-01-20 15:02:34 +0100

    ExceptionTest added

---
+++ tests/Objects/ExceptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SocialMedia\Facebook\tests\Objects;
+
+// required to load
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use SocialMedia\Facebook\Objects\Exception as FacebookException;
+
+/**
+ * Exception Class which throws a Facebook exception.
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test service
+     */
+    public function testException()
+    {
+        throw new FacebookException('Exception correctly thrown in our test.');
+    }
+}
